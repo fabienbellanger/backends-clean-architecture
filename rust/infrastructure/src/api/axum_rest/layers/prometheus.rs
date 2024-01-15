a6f78905531a8f91449ff03eef9baf869ef433d8
Fabien Bellanger <fabien.bellanger@apitic.com>
2024-01-15 11:52:57 +0100

    Update deps

--- rust/infrastructure/src/api/axum_rest/layers/prometheus.rs
+++ rust/infrastructure/src/api/axum_rest/layers/prometheus.rs
@@ -6,7 +6,7 @@ use axum::{extract::MatchedPath, middleware::Next, response::IntoResponse};
 use clean_architecture_shared::api_error;
 use clean_architecture_shared::error::{ApiError, ApiErrorCode, ApiResult};
 use hyper::Request;
-use metrics::{histogram, increment_counter};
+use metrics::{counter, histogram};
 use metrics_exporter_prometheus::{Matcher, PrometheusBuilder, PrometheusHandle};
 use std::time::Instant;
 
@@ -48,8 +48,9 @@ impl PrometheusMetric {
             ("status", status),
         ];
 
-        increment_counter!("http_requests_total", &labels);
-        histogram!("http_requests_duration_seconds", latency, &labels);
+        counter!("http_requests_total", &labels);
+        let histogram = histogram!("http_requests_duration_seconds", &labels);
+        histogram.record(latency);
 
         response
     }
